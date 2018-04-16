a1005c7786c31d7ba162b6c075ba3822da1f6ff9
Alexandre PETRILLO <alexandre.petrillo@gmail.com>
2018-04-16 11:37:29 +0200

    dto

---
+++ src/main/java/com/example/demo/dto/ClientDTO.java
@@ -0,0 +1,28 @@
+package com.example.demo.dto;
+
+
+/**
+ * Created by Kayne on 09/04/2018.
+ */
+public class ClientDTO {
+
+    private String prenom;
+
+    private String nom;
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+}
