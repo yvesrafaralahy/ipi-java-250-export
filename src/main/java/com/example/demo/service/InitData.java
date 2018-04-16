aa1d00b47c0b9a74663f1185904b7fb23fd530b1
Alexandre PETRILLO <alexandre.petrillo@gmail.com>
2018-04-16 11:16:41 +0200

    1st service

---
+++ src/main/java/com/example/demo/service/InitData.java
@@ -0,0 +1,43 @@
+package com.example.demo.service;
+
+import com.example.demo.entity.Article;
+import com.example.demo.entity.Client;
+import com.example.demo.entity.Facture;
+import com.example.demo.entity.LigneFacture;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+
+@Service
+@Transactional
+public class InitData {
+
+    @Autowired
+    private EntityManager em;
+
+    public void insertTestData() {
+
+        Client client  = new Client();
+        client.setNom("PETRILLO");
+        client.setPrenom("Alexandre");
+        em.persist(client);
+
+        Article article = new Article();
+        article.setLibelle("Carte mère ASROCK 2345");
+        article.setPrix(79.90);
+        em.persist(article);
+
+        Facture facture = new Facture();
+        facture.setClient(client);
+        em.persist(facture);
+
+        LigneFacture ligneFacture1 = new LigneFacture();
+        ligneFacture1.setFacture(facture);
+        ligneFacture1.setArticle(article);
+        ligneFacture1.setQuantite(1);
+        em.persist(ligneFacture1);
+
+    }
+}
