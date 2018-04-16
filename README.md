7efb27b24e74393f8f12922786e5d077b40b647c
Alexandre PETRILLO <alexandre.petrillo@gmail.com>
2018-04-16 10:56:59 +0200

    entity

--- README.md
+++ README.md
@@ -1,6 +1,27 @@
-Export
-CSV
-XLSX
-JSON
-XML
-PDF
+# Export
+
+## CSV
+Un fichier CSV est un fichier texte "à plat" chaque ligne correspondant à une ligne du tableau, chaque colonne est séparé par des ";"
+Le contenu d'une cellule peut être entouré de double quote
+
+## CSV amélioré
+Créer un objet permettant d'améliorer l'export.
+
+## XLSX
+Workbook workbook = new XSSFWorkbook();
+Sheet sheet = workbook.createSheet("Clients");
+Row headerRow = sheet.createRow(0);
+Cell cellPrenom = headerRow.createCell(0);
+cellPrenom.setCellValue("Prénom");
+workbook.write(fileOutputStream);
+workbook.close();
+
+## JSON
+Utilisation de JACKSON
+Créer un servie et lui inject un ObjectMapper
+
+## XML
+
+## PDF
+
+
---
+++ src/main/java/com/example/demo/entity/Facture.java
@@ -0,0 +1,46 @@
+package com.example.demo.entity;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by Kayne on 09/04/2018.
+ */
+@Entity
+public class Facture {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @ManyToOne
+    private Client client;
+
+    @OneToMany(mappedBy = "facture")
+    private Set<LigneFacture> ligneFactures = new HashSet<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
+    public Set<LigneFacture> getLigneFactures() {
+        return ligneFactures;
+    }
+
+    public void setLigneFactures(Set<LigneFacture> ligneFactures) {
+        this.ligneFactures = ligneFactures;
+    }
+}
